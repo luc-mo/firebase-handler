ff64b5922f9830883b740403de16da04af6a9ff3
SnowDrive <luc.moralesr@gmail.com>
2023-10-03 04:00:34 -0300

    feat: add handlers config to app config types, add firestore settings

--- src/types/config.d.ts
+++ src/types/config.d.ts
@@ -1,10 +1,17 @@
-// Configuration and logger interfaces
+import type { FirestoreSettings } from '@/types/firebase'
+
 export interface FirebaseConfig {
 	databaseURL: string
 	storageBucket: string
 }
 
+export interface HandlersConfig {
+	useFirestoreRecommendedSettings: boolean
+}
+
 export interface AppConfig {
 	firebase?: Partial<FirebaseConfig>
+	firestore?: Partial<FirestoreSettings>
+	handlers?: Partial<HandlersConfig>
 	[key: string]: any
 }

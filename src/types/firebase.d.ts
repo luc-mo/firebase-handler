a869e2c050d9390724963b00aa9fbdb7d89acf5e
SnowDrive <luc.moralesr@gmail.com>
2023-10-03 03:46:52 -0300

    feat: add firestore settings types

--- src/types/firebase.d.ts
+++ src/types/firebase.d.ts
@@ -1,6 +1,8 @@
 // Firebase Admin SDK types
 export type FirebaseAdmin = typeof import('firebase-admin')
 export type FirebaseApp = import('firebase-admin').app.App
-export type FirestoreDb = import('firebase-admin').firestore.Firestore
 export type RealtimeDb = import('firebase-admin').database.Database
 export type StorageDb = import('firebase-admin').storage.Storage
+
+export type FirestoreDb = import('firebase-admin').firestore.Firestore
+export type FirestoreSettings = import('firebase-admin').firestore.Settings

7bf99419b997e05a479507465b8bd53d3cc7eeda
SnowDrive <luc.moralesr@gmail.com>
2023-10-03 03:33:39 -0300

    feat: add firebase-admin type exports

---
+++ src/types/firebase.d.ts
@@ -0,0 +1,6 @@
+// Firebase Admin SDK types
+export type FirebaseAdmin = typeof import('firebase-admin')
+export type FirebaseApp = import('firebase-admin').app.App
+export type FirestoreDb = import('firebase-admin').firestore.Firestore
+export type RealtimeDb = import('firebase-admin').database.Database
+export type StorageDb = import('firebase-admin').storage.Storage

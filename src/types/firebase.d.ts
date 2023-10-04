ee70c6c400e1e69ddd8966ebed673408489cebe0
SnowDrive <luc.moralesr@gmail.com>
2023-10-03 23:46:16 -0300

    feat: add service account and refresh token types

--- src/types/firebase.d.ts
+++ src/types/firebase.d.ts
@@ -1,8 +1,12 @@
 // Firebase Admin SDK types
 export type FirebaseAdmin = typeof import('firebase-admin')
+
 export type FirebaseApp = import('firebase-admin').app.App
-export type RealtimeDb = import('firebase-admin').database.Database
-export type StorageDb = import('firebase-admin').storage.Storage
+export type ServiceAccount = string | import('firebase-admin').ServiceAccount
+export type RefreshToken = string | object
 
 export type FirestoreDb = import('firebase-admin').firestore.Firestore
 export type FirestoreSettings = import('firebase-admin').firestore.Settings
+
+export type RealtimeDb = import('firebase-admin').database.Database
+export type StorageDb = import('firebase-admin').storage.Storage
